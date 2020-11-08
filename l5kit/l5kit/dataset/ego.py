641b50f90afc8290193d6802549452bd7a5cfe0d
Louis Yang <18520676+louis925@users.noreply.github.com>
2020-11-08 02:10:47 -0800

    less dependency on pytorch
    to reduce the memory usage from not import everything from torch

--- l5kit/l5kit/dataset/ego.py
+++ l5kit/l5kit/dataset/ego.py
@@ -4,7 +4,7 @@ from functools import partial
 from typing import Optional
 
 import numpy as np
-from torch.utils.data import Dataset
+# from torch.utils.data import Dataset  # to avoid loading the entire pytorch package
 
 from ..data import (
     ChunkedDataset,
@@ -15,6 +15,33 @@ from ..data import (
 from ..kinematic import Perturbation
 from ..rasterization import Rasterizer, RenderContext
 from ..sampling import generate_agent_sample
+
+
+class Dataset:
+    r"""An abstract class representing a :class:`Dataset`.
+
+    All datasets that represent a map from keys to data samples should subclass
+    it. All subclasses should overwrite :meth:`__getitem__`, supporting fetching a
+    data sample for a given key. Subclasses could also optionally overwrite
+    :meth:`__len__`, which is expected to return the size of the dataset by many
+    :class:`~torch.utils.data.Sampler` implementations and the default options
+    of :class:`~torch.utils.data.DataLoader`.
+
+    .. note::
+      :class:`~torch.utils.data.DataLoader` by default constructs a index
+      sampler that yields integral indices.  To make it work with a map-style
+      dataset with non-integral indices/keys, a custom sampler must be provided.
+    """
+
+    def __getitem__(self, index):
+        raise NotImplementedError
+
+    # def __add__(self, other: 'Dataset[T_co]') -> 'ConcatDataset[T_co]':
+    #     return ConcatDataset([self, other])
+
+    # No `def __len__(self)` default?
+    # See NOTE [ Lack of Default `__len__` in Python Abstract Base Classes ]
+    # in pytorch/torch/utils/data/sampler.py
 
 
 class EgoDataset(Dataset):
